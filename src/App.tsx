c943da311a4bb753cbae2be2e7328c668737ed5d
Arun Kumar <arun.kumar@cubiclogics.com>
2024-09-27 16:21:55 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PrimaryButton } from "@fluentui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex">
+      <h1>Employee Matrix</h1>
+      <PrimaryButton>Submit</PrimaryButton>
+    </div>
+  );
+}
+
+export default App;
